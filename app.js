43237ac5a94e8deb4955e6a0274ea1f279c762f4
clemsos <clement.renaud@gmail.com>
2013-03-05 15:27:28 +0800

    missing files

---
+++ app.js
@@ -0,0 +1,2 @@
+window.d3container =  require('./d3container');
+console.log('Requires loaded - compiled by Browserify');
---
+++ feedconfig.browser.js
@@ -0,0 +1,169 @@
+function FeedConfig(data) {
+    var self = this;
+
+    self.validGender = [
+      'Both',
+      'Men',
+      'Women'
+    ];
+
+    self.validAgeRange = [
+      'All',
+      '18-',
+      '24-',
+      '35-',
+      '40+'
+    ];
+
+    self.validTier = [
+      'Tier1',
+      'Tier2',
+      'Tier3',
+      'All'
+    ];
+
+    self.validSampling = [
+      "second",
+      "minute",
+      "hour",
+      "day",
+      "month",
+      "year"
+    ];
+
+    self.setType = function(type) {
+      if(type !== 'historic' && type !== 'realtime')
+        throw new Error('Invalid type ' + type);
+      self.type = type;
+    };
+
+    self.setGender = function(gender) {
+      if(self.validGender.indexOf(gender) == -1)
+        throw new Error('Invalid gender: ' + gender)
+      self.gender = gender;
+
+    };
+
+    self.setAgeRange = function(age) {
+      if(self.validAgeRange.indexOf(age) == -1)
+        throw new Error('Invalid age range: ' + age)
+      self.age = age;
+    
+    };
+
+    self.setTier = function(tier) {
+      if(self.validTier.indexOf(tier) == -1)
+        throw new Error('Invalid tier: ' + tier)
+      self.tier = tier;
+    };
+
+    self.setSampling = function(sampling) {
+      if(self.validSampling.indexOf(sampling) == -1)
+        throw new Error('Invalid sampling: ' + sampling)
+      self.sampling = sampling;
+    };
+
+    self.setStart = function(start) {
+      if(!moment(start).isValid())
+        throw new Error("Invalid datetime: " + start);
+      self.start = start;
+    }
+
+    self.setEnd = function(end) {
+      if(!moment(end).isValid())
+        throw new Error("Invalid datetime: " + end);
+      self.end = end;
+    }
+
+    self.setNumberItems = function(numberItems) {
+      if(!(numberItems > 0))
+        throw new Error("Invalid number of items: " + numberItems);
+      self.numberItems = numberItems;
+    }
+
+    self.setDemographics = function(gender, age, tier) {
+      self.setGender(gender);
+      self.setAgeRange(age);
+      self.setTier(tier);
+    };
+
+    self.setWordCount = function(count) {
+      self.count = count;
+    };
+
+    self.setHistoric = function(start, end, sampling) {
+      self.setType('historic');
+      self.setStart(start);
+      self.setEnd(end);
+      self.setSampling(sampling);
+    };
+
+    self.setRealtime = function(sampling, numberItems) {
+      self.setType('realtime');
+      self.setNumberItems(numberItems);
+      self.setSampling(sampling);
+    };
+
+    self.toJSON = function() {
+      if(self.type === 'historic') {
+        return JSON.stringify({
+          gender: self.gender,
+          tier: self.tier,
+          age: self.age,
+          start: self.start,
+          end: self.end,
+          sampling: self.sampling,
+          count: self.count
+        });
+      } else if(self.type === 'realtime') {
+        return JSON.stringify({
+          gender: self.gender,
+          tier: self.tier,
+          age: self.age,
+          numberItems: self.numberItems,
+          sampling: self.sampling,
+          count: self.count
+        });
+      } else {
+        throw new Error('not configured for historic data');
+      }
+    };
+
+    self.isHistoric = function() {
+      return self.type === 'historic';
+    }
+
+    self.isRealtime = function() {
+      return self.type === 'realtime';
+    }
+
+    self.setData = function(data) {
+      if(data) {
+        var data = JSON.parse(data);
+        if(data.start) {
+          self.setType('historic');
+          self.setStart(data.start);
+          self.setEnd(data.end);
+        } else {
+          self.setType('realtime');
+          self.setNumberItems(data.numberItems);
+        }
+        self.setGender(data.gender);
+        self.setAgeRange(data.age);
+        self.setTier(data.tier);
+        self.setSampling(data.sampling);
+        self.setWordCount(data.count);
+      } else {
+        self.type = null;
+        self.gender = null;
+        self.age = null;
+        self.tier = null;
+        self.count = null;
+        self.start = null;
+        self.end = null;
+      }
+    }
+    self.setData(data);
+
+    return self;
+}
