d1d591cfd91f35f870511e0ced6b37f83fced701
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-05 08:08:33 +0800

    group feedconfig and d3container

---
+++ index.js
@@ -0,0 +1,5 @@
+var d3container = require('./d3container.js');
+var feedconfig = require('./feedconfig.js');
+
+exports.D3Container =  d3container.D3Container;
+exports.FeedConfig =  feedconfig.FeedConfig;
