472ff27c0a5b02c0ea98136f3c168ea34053a721
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-08 09:53:00 +0800

    regenerated bundle

--- bundle.js
+++ bundle.js
@@ -3,7 +3,88 @@ window.feedconfig =  require('./feedconfig');
 window.slicer =  require('./slice');
 
 console.log('Requires loaded - compiled by Browserify');
-},{"./d3container":2,"./feedconfig":3,"./slice":4}],5:[function(require,module,exports){var events = require('events');
+},{"./feedconfig":2,"./d3container":3,"./slice":4}],4:[function(require,module,exports){'use strict';
+
+function Slice(data) {
+    var self = this;
+    self.words = null;
+    self.time = null;
+    if(data) {
+      var other;
+      if(typeof data === 'string')
+        other = JSON.parse(data);
+      else if(typeof data === 'object')
+        other = data;
+      else
+        throw new Error('Unsupported data type ' + typeof data);
+
+      self.words = other.words;
+      self.time = other.time;
+    }
+
+    self.addValue = function(word, count, source, panel) {
+      if(self.words === null)
+        self.words = [];
+      self.words.push({word:word, count:count, source:source, panel:panel});
+    };
+
+    self.setTime = function(t) {
+      self.time = t;
+    };
+
+    self.getTime = function(t) {
+      return self.time;
+    };
+
+    self.equals = function(other) {
+      self.checkValid();
+      other.checkValid();
+
+      if(self.time !== other.time)
+        return false;
+      if(self.words.length !== other.words.length)
+        return false;
+
+      for(var v in self.words) {
+        if(self.words[v].word !== other.words[v].word)
+          return false;
+        if(self.words[v].count !== other.words[v].count)
+          return false;
+        if(self.words[v].source !== other.words[v].source)
+          return false;
+        if(self.words[v].panel !== other.words[v].panel)
+          return false;
+      }
+
+      return true;
+    };
+
+    self.checkValid = function() {
+      if(!(self.words instanceof Array))
+        throw new Error('Values should be array, is ' + self.words);
+      if(!(typeof self.time === 'string'))
+        throw new Error('Timestamp should be string, is: ' + self.time);
+      if((self.words.length === 0))
+        throw new Error('Empty array of words ');
+    };
+
+    self.toJSON = function() {
+      self.checkValid();
+      return JSON.stringify({
+        time: self.time,
+        words: self.words
+      });
+    }
+
+    self.next = function() {
+      return self.words.pop();
+    };
+
+    return self;
+}
+
+exports.Slice = Slice;
+},{}],5:[function(require,module,exports){var events = require('events');
 
 exports.isArray = isArray;
 exports.isDate = function(obj){return Object.prototype.toString.call(obj) === '[object Date]'};
@@ -593,104 +674,7 @@ EventEmitter.prototype.listeners = function(type) {
 };
 
 })(require("__browserify_process"))
-},{"__browserify_process":7}],2:[function(require,module,exports){"use strict";
-
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
-var slice = require('./slice.js');
-var moment = require('moment');
-
-function D3Container(desiredNumberOfXValues) {
-  var self = this;
-
-  self.d3 = [];
-  self.keyToIndexes = {};
-  self.xIndexes = {};
-  self.xValues = [];
-  self.maxIndex = 0;
-  self.desiredNumberOfXValues = desiredNumberOfXValues;
-
-  self.getKeyIndex = function(key) {
-    // if does not exist, initialize
-    if(self.keyToIndexes[key] === undefined) {
-      self.keyToIndexes[key] = {};
-      self.keyToIndexes[key].keyIndex = self.d3.length;
-      self.keyToIndexes[key].xIndexes = {};
-      self.d3.push({key: key, values: []});
-    }
-    return self.keyToIndexes[key].keyIndex;
-  };
-
-  self.getXIndex = function(key, keyIndex, x) {
-    if(self.xIndexes[x] === undefined) {
-      self.xIndexes[x] = self.maxIndex++;
-      self.xValues.push({x: x, index: self.xIndexes[x]});
-    }
-    if(self.d3[keyIndex].values[self.xIndexes[x]] === undefined) {
-      self.d3[keyIndex].values[self.xIndexes[x]] = {x:-1, y:-1};
-    }
-    return self.xIndexes[x];
-  };
-
-  // key is word
-  // x is timestamp
-  // y is count
-  self.update = function(key, x, y) {
-    var keyIndex = self.getKeyIndex(key);
-    var xIndex = self.getXIndex(key, keyIndex, x);
-    self.d3[keyIndex].values[xIndex].x = x;
-    self.d3[keyIndex].values[xIndex].y = y;
-    if(!self.desiredNumberOfXValues || self.xValues.length >= self.desiredNumberOfXValues) {
-      this.emit('updated');
-    }
-  };
-
-  self.updateSlice = function(s) {
-    if(!(s instanceof slice.Slice))
-      throw new Error('not a slice');
-    var v;
-    while(v = s.next()) {
-      self.update(v.word, s.getTime(), v.count);
-    }
-  };
-
-  self.current = function() {
-    self.xValues.sort(function(a,b) {
-      return a.x - b.x;
-    });
-
-    if(self.desiredNumberOfXValues && self.xValues.length > self.desiredNumberOfXValues) {
-      self.xValues.splice(0, self.xValues.length - self.desiredNumberOfXValues);
-    }
-
-    var newD3 = [];
-    self.d3.forEach(function(d3, j) {
-        var o = {key: d3.key, values: []};
-        newD3.push(o);
-        self.xValues.forEach(function(v, i) {
-          if(d3.values[v.index] === undefined)
-            o.values.push({x: v.x, y: 0});
-          else
-            o.values.push(d3.values[v.index]);
-        });
-    });
-    return newD3;
-    //return self.d3;
-  }
-
-  self.setXValues = function(newValue) {
-
-    self.desiredNumberOfXValues = newValue;
-
-  }
-
-  return self;
-}
-
-util.inherits(D3Container, EventEmitter)
-
-exports.D3Container = D3Container;
-},{"util":5,"events":6,"./slice.js":4,"moment":8}],3:[function(require,module,exports){var moment = require('moment');
+},{"__browserify_process":7}],2:[function(require,module,exports){var moment = require('moment');
 
 function FeedConfig(data) {
     var self = this;
@@ -862,88 +846,112 @@ function FeedConfig(data) {
 }
 
 exports.FeedConfig = FeedConfig;
-},{"moment":8}],4:[function(require,module,exports){'use strict';
+},{"moment":8}],3:[function(require,module,exports){"use strict";
 
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var slice = require('./slice.js');
 var moment = require('moment');
 
-function Slice(data) {
-    var self = this;
-    self.words = null;
-    self.time = null;
-    if(data) {
-      var other;
-      if(typeof data === 'string')
-        other = JSON.parse(data);
-      else if(typeof data === 'object')
-        other = data;
-      else
-        throw new Error('Unsupported data type ' + typeof data);
+function D3Container(desiredNumberOfXValues) {
+  var self = this;
 
-      self.words = other.words;
-      self.time = other.time;
+  self.d3 = [];
+  self.keyToIndexes = {};
+  self.xIndexes = {};
+  self.xValues = [];
+  self.maxIndex = 0;
+  self.desiredNumberOfXValues = desiredNumberOfXValues;
+
+  self.getKeyIndex = function(key) {
+    // if does not exist, initialize
+    if(self.keyToIndexes[key] === undefined) {
+      self.keyToIndexes[key] = {};
+      self.keyToIndexes[key].keyIndex = self.d3.length;
+      self.keyToIndexes[key].xIndexes = {};
+      self.d3.push({key: key, values: []});
+    }
+    return self.keyToIndexes[key].keyIndex;
+  };
+
+  self.getXIndex = function(key, keyIndex, x) {
+    if(self.xIndexes[x] === undefined) {
+      self.xIndexes[x] = self.maxIndex++;
+      self.xValues.push({x: x, index: self.xIndexes[x]});
+    }
+    if(self.d3[keyIndex].values[self.xIndexes[x]] === undefined) {
+      self.d3[keyIndex].values[self.xIndexes[x]] = {x:-1, y:-1};
+    }
+    return self.xIndexes[x];
+  };
+
+  // key is word
+  // x is timestamp in unix time
+  // y is count
+  self.update = function(key, x, y) {
+    var keyIndex = self.getKeyIndex(key);
+    var xIndex = self.getXIndex(key, keyIndex, x);
+    self.d3[keyIndex].values[xIndex].x = x;
+    self.d3[keyIndex].values[xIndex].y = y;
+    if(!self.desiredNumberOfXValues || self.xValues.length >= self.desiredNumberOfXValues) {
+      this.emit('updated');
+    }
+  };
+
+  self.updateSlice = function(s) {
+    if(!(s instanceof slice.Slice))
+      throw new Error('not a slice');
+    var v;
+    while(v = s.next()) {
+      var unixTime = moment(s.getTime()).unix();
+      self.update(v.word, unixTime, v.count);
+    }
+  };
+
+  self.current = function() {
+    // xValues contain all possible x (timestamp)
+    self.xValues.sort(function(a,b) {
+      return a.x - b.x;
+    });
+
+    // if we already have more than what this container needs, trim the list
+    if(self.desiredNumberOfXValues && self.xValues.length > self.desiredNumberOfXValues) {
+      self.xValues.splice(0, self.xValues.length - self.desiredNumberOfXValues);
     }
 
-    self.addValue = function(word, count, source, panel) {
-      if(self.words === null)
-        self.words = [];
-      self.words.push({word:word, count:count, source:source, panel:panel});
-    };
+    // create the [key:, values: [x:, y:]] datastructure
+    var newD3 = [];
+    self.d3.forEach(function(d3, j) {
+        var o = {key: d3.key, values: []};
+        newD3.push(o);
+        // for each of the potential x values
+        self.xValues.forEach(function(v, i) {
+          // if it doesn't exist, add it but with a y of zero
+          if(d3.values[v.index] === undefined) {
+            o.values.push({x: v.x, y: 0});
+          }
+          // else, use the x:, y: value as is
+          else {
+            o.values.push({x: d3.values[v.index].x, y: d3.values[v.index].y});
+          }
+        });
+    });
+    return newD3;
+  }
 
-    self.setTime = function(t) {
-      self.time = t;
-    };
+  self.setXValues = function(newValue) {
 
-    self.getTime = function(t) {
-      return self.time;
-    };
+    self.desiredNumberOfXValues = newValue;
 
-    self.equals = function(other) {
-      self.checkValid();
-      other.checkValid();
+  }
 
-      if(self.time !== other.time)
-        return false;
-      if(self.words.length !== other.words.length)
-        return false;
-
-      for(var v in self.words) {
-        if(self.words[v].word !== other.words[v].word)
-          return false;
-        if(self.words[v].count !== other.words[v].count)
-          return false;
-        if(self.words[v].source !== other.words[v].source)
-          return false;
-        if(self.words[v].panel !== other.words[v].panel)
-          return false;
-      }
-
-      return true;
-    };
-
-    self.checkValid = function() {
-      if(!self.words instanceof Array)
-        throw new Error('Values should be array');
-      if(!self.time instanceof String)
-        throw new Error('Timestamp should be string');
-    };
-
-    self.toJSON = function() {
-      self.checkValid();
-      return JSON.stringify({
-        time: self.time,
-        words: self.words
-      });
-    }
-
-    self.next = function() {
-      return self.words.pop();
-    };
-
-    return self;
+  return self;
 }
 
-exports.Slice = Slice;
-},{"moment":8}],8:[function(require,module,exports){(function(){// moment.js
+util.inherits(D3Container, EventEmitter)
+
+exports.D3Container = D3Container;
+},{"util":5,"events":6,"./slice.js":4,"moment":8}],8:[function(require,module,exports){(function(){// moment.js
 // version : 2.0.0
 // author : Tim Wood
 // license : MIT
