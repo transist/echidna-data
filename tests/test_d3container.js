126268341d17c9c9ec59d9f7059f08db598a6b2c
Ricky Ng-Adam <rngadam@gmail.com>
2013-04-02 13:53:33 +0800

    fixes issue #2: added reset method and object modification

--- tests/test_d3container.js
+++ tests/test_d3container.js
@@ -11,31 +11,36 @@ describe('d3container', function() {
     var source = 'http://somewhere';
     var panel = 5;
     var timestamp = moment().utc();
+    var current;
 
-    it('updates correctly single value', function() {
-      var updater = new d3container.D3Container();
+    beforeEach(function() {
+      current = [];
+    });
+
+    it('update correctly with single value', function() {
+      var updater = new d3container.D3Container(current);
       updater.update('word', 'xvalue', 1);
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify([{key: 'word', values: [{x: 'xvalue', y:1}]}]));
     });
 
     it('re-updates correctly single value', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
       updater.update('word', 'xvalue', 1);
       updater.update('word', 'xvalue', 2);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify([{key: 'word', values: [{x: 'xvalue', y:2}]}]));
     });
 
     it('updates correctly multiple x,y values', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
 
       updater.update('word1', 1, 10);
       updater.update('word1', 2, 20);
       updater.update('word1', 3, 30);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -51,15 +56,15 @@ describe('d3container', function() {
         ));
     });
 
-    it('updates correctly multiple x,y values', function() {
-      var updater = new d3container.D3Container();
+    it('updates correctly multiple words with multiple x,y values', function() {
+      var updater = new d3container.D3Container(current);
 
       updater.update('word1', 1, 10);
       updater.update('word1', 2, 20);
       updater.update('word1', 3, 30);
       updater.update('word2', 3, 40);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -85,13 +90,13 @@ describe('d3container', function() {
     });
 
     it('out-of-order updates on x (time) still give proper sorted results', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
 
       updater.update('word1', 3, 30);
       updater.update('word1', 2, 20);
       updater.update('word1', 1, 10);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -108,11 +113,11 @@ describe('d3container', function() {
     });
 
     it('update, current, update, current cycle works', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
 
       updater.update('word1', 1, 30);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -128,7 +133,7 @@ describe('d3container', function() {
 
       updater.update('word1', 2, 40);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -145,14 +150,14 @@ describe('d3container', function() {
     });
 
     it('parametrizable number of values', function() {
-      var updater = new d3container.D3Container(3);
+      var updater = new d3container.D3Container(current, 3);
 
       updater.update('word1', 1, 10);
       updater.update('word1', 2, 20);
       updater.update('word1', 3, 30);
       updater.update('word1', 4, 40);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -170,7 +175,7 @@ describe('d3container', function() {
     });
 
     it('emit event when reach target number of values', function(done) {
-      var updater = new d3container.D3Container(4);
+      var updater = new d3container.D3Container(current, 4);
       updater.on('updated', function() { done(); });
 
       updater.update('word1', 1, 10);
@@ -181,20 +186,20 @@ describe('d3container', function() {
     });
 
     it('emit event every update when no target value', function(done) {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
       updater.on('updated', function() { done(); });
       updater.update('word1', 1, 10);
     });
 
     it('can update from a slice', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
 
       s1.setTime(timestamp.toJSON());
       s1.addValue(word, count, source, panel);
 
       updater.updateSlice(s1);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -210,14 +215,14 @@ describe('d3container', function() {
     });
 
     it('can update from a slice with a partial timestamp', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
       var partial_timestamp = '2013-03-12T01';
       s1.setTime(partial_timestamp);
       s1.addValue(word, count, source, panel);
 
       updater.updateSlice(s1);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -234,7 +239,7 @@ describe('d3container', function() {
 
 
     it('can update from two slices', function() {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
       var s1 = new slice.Slice();
       var s2 = new slice.Slice();
       var timestamp1 = moment().utc();
@@ -249,7 +254,7 @@ describe('d3container', function() {
       updater.updateSlice(s1);
       updater.updateSlice(s2);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -267,8 +272,8 @@ describe('d3container', function() {
       );
     });
 
-    it('can update with one slice that has no words', function() {
-      var updater = new d3container.D3Container();
+    it('reset between two slices only result in second slice', function() {
+      var updater = new d3container.D3Container(current);
       var s1 = new slice.Slice();
       var s2 = new slice.Slice();
       var timestamp1 = moment().utc();
@@ -278,11 +283,47 @@ describe('d3container', function() {
       s1.addValue(word, count, source, panel);
 
       s2.setTime(timestamp2.toJSON());
+      s2.addValue(word, count, source, panel);
+
+      updater.updateSlice(s1);
+      updater.reset()
+      updater.updateSlice(s2);
+
+      JSON.stringify(current).should.equal(
+        JSON.stringify(
+          [
+            {
+              key: word,
+              values:
+              [
+                // moment.valueOf : milliseconds
+                // moment.unix() : seconds
+                {x: timestamp2.valueOf(), y:count},
+              ]
+            },
+          ]
+        )
+      );
+    });
+
+    it('can update with one slice that has no words', function() {
+      var updater = new d3container.D3Container(current);
+      var s1 = new slice.Slice();
+      var s2 = new slice.Slice();
+      var timestamp1 = moment().utc();
+      var timestamp2 = moment();
+      timestamp2.add('second', 1).utc();
+
+      s1.setTime(timestamp1.toJSON());
+      s1.addValue(word, count, source, panel);
+
+      s2.setTime(timestamp2.toJSON());
+      // NO WORDS IN s2
 
       updater.updateSlice(s1);
       updater.updateSlice(s2);
 
-      JSON.stringify(updater.current()).should.equal(
+      JSON.stringify(current).should.equal(
         JSON.stringify(
           [
             {
@@ -300,8 +341,14 @@ describe('d3container', function() {
       );
     });
 
+    it('reset on empty object', function() {
+      var updater = new d3container.D3Container(current);
+      updater.reset();
+      JSON.stringify(current).should.equal(JSON.stringify([]));
+    });
+
     it('fails on invalid parameter that is not slice.Slice', function(done) {
-      var updater = new d3container.D3Container();
+      var updater = new d3container.D3Container(current);
       try {
         updater.updateSlice("String object");
       } catch(e) {
@@ -318,16 +365,5 @@ describe('d3container', function() {
     it('moment and Date.parse compatible', function() {
       var t = moment().utc();
       t.valueOf().should.equal(Date.parse(t.toJSON()));
-    });
-
-    it('test that current() only returns new objects', function() {
-      var updater = new d3container.D3Container(1);
-      updater.update('word1', 1, 10);
-      var current = updater.current();
-
-      current[0].values[0].x.should.equal(1);
-      current[0].values[0].marker = true;
-      var current2 = updater.current();
-      should.equal(current2[0].values[0].marker, undefined);
     });
 });
